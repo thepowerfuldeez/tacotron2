6839b64124211c79812d8d6af6d952194a3d64a0
Georgiy <deezchannel@gmail.com>
2019-12-16 12:45:14 +0000

    fixes for distributed training

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import argparse
 import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
@@ -11,14 +12,14 @@ from model import Model
 from data import TTSDataset, TTSCollate
 
 
-def train(model, loader, opt, device="cuda", iteration=0, log_every=100, fp16=False):
+def train(model, loader, opt, local_rank=0, iteration=0, log_every=100, fp16=False):
     """Train loop"""
-    min_metric = 1e3
+    metric_values = []
     for text, input_lengths, mel, stop_target in loader:
-        text = text.to(device)
-        input_lengths = input_lengths.to(device)
-        mel = mel.to(device)
-        stop_target = stop_target.to(device)
+        text = text.to("cuda", non_blocking=True)
+        input_lengths = input_lengths.to("cuda", non_blocking=True)
+        mel = mel.to("cuda", non_blocking=True)
+        stop_target = stop_target.to("cuda", non_blocking=True)
         iteration += 1
 
         opt.zero_grad()
@@ -26,26 +27,29 @@ def train(model, loader, opt, device="cuda", iteration=0, log_every=100, fp16=Fa
         mel_loss = F.mse_loss(mel_pred, mel)
         mel_postnet_loss = F.mse_loss(mel_pred_postnet, mel)
         stop_loss = F.binary_cross_entropy(stop_predictions, stop_target)
-        if iteration % log_every == 0:
-            print(f"{iteration}, mel_loss.item()={mel_loss.item():.2f}, mel_postnet_loss.item()={mel_postnet_loss.item():.2f}, stop_loss.item()={stop_loss.item():.3f}")
+        
         loss = mel_loss + mel_postnet_loss + stop_loss
-        min_metric = min(min_metric, loss.item())
+        metric_values.append(loss.item())
+        
+        # print only if it's first device (local_rank == 0)
+        if iteration % log_every == 0 and local_rank == 0:
+            print(f"{iteration}, mel_loss.item()={mel_loss.item():.2f}, mel_postnet_loss.item()={mel_postnet_loss.item():.2f}, stop_loss.item()={stop_loss.item():.3f}")
         if fp16:
             with amp.scale_loss(loss, opt) as scaled_loss:
                 scaled_loss.backward()
         else:
             loss.backward()
         opt.step()
-    return iteration, min_metric
+    return iteration, np.mean(metric_values)
 
 
-def test_alignment(batch, model, device):
+def test_alignment(batch, model):
     """For use in jupyter notebook or for tensorboard logging"""
     text, input_lengths, mel, stop_target = TTSCollate()(batch)
     with torch.no_grad():
-        text = text.to(device)
-        input_lengths = input_lengths.to(device)
-        mel = mel.to(device)
+        text = text.to("cuda", non_blocking=True)
+        input_lengths = input_lengths.to("cuda", non_blocking=True)
+        mel = mel.to("cuda", non_blocking=True)
         mel_pred, mel_pred_postnet, stop_predictions, alignment = model(text, input_lengths, mel)
     plt.imshow(alignment.cpu().numpy()[0][::-1], aspect='auto')
     plt.show()
@@ -61,12 +65,14 @@ def save_checkpoint(model, iteration, out_path, fp16):
     torch.save(checkpoint, out_path)
 
 
-def load_checkpoint(checkpoint_path, model, fp16):
-    checkpoint = torch.load(checkpoint_path)
+def load_checkpoint(checkpoint_path, model, fp16, local_rank):
+    # we should load checkpoint on appropriate gpu device, derived from local_rank, use lambda (example from
+    # https://github.com/NVIDIA/apex/blob/master/examples/imagenet/main_amp.py )
+    checkpoint = torch.load(checkpoint_path, map_location=lambda storage, loc: storage.cuda(local_rank))
     model.load_state_dict(checkpoint['state_dict'])
     if fp16 and 'amp' in checkpoint:
         amp.load_state_dict(checkpoint['amp'])
-    return checkpoint['iteration']
+    return checkpoint['iteration'], checkpoint['best_metric']
 
 
 def parse_args():
@@ -74,7 +80,6 @@ def parse_args():
     p.add_argument("--ds-path", type=Path, help="Dataset metadata path")
     p.add_argument("--checkpoints-path", type=Path)
     p.add_argument("--checkpoint", default=None)
-    p.add_argument("--device", default="cuda")
 
     p.add_argument("--batch-size", type=int, default=48)
     p.add_argument("--num-epochs", type=int, default=1000)
@@ -83,7 +88,7 @@ def parse_args():
 
     p.add_argument("--fp16", action="store_true")
     p.add_argument("--distributed", help="DDP training", action="store_true")
-    p.add_argument("--local_rank", help="for DDP", default=0, type=int)
+    p.add_argument("--local_rank", help="for DDP, default device", default=0, type=int)
     p.add_argument("--sync-bn", help="synchronized batchnorm for distributed", action="store_true")
     return p.parse_args()
 
@@ -91,7 +96,6 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
 
-    device = args.device
     checkpoints_path = args.checkpoints_path
     checkpoint = args.checkpoint
     ds_path = args.ds_path
@@ -100,14 +104,31 @@ if __name__ == "__main__":
     log_every = args.log_every
     save_every = args.save_every
     fp16 = args.fp16
+    local_rank = args.local_rank
+    
+    ds = TTSDataset(ds_path)
 
     model = Model(fp16=fp16)
-    model = model.to(device)
+    model = model.to(local_rank)  # move model to appropriate gpu when using distributed training
     iteration = 0
+    best_metric = 1e3
     if checkpoint:
-        iteration = load_checkpoint(checkpoint, model, fp16)
+        iteration, best_metric = load_checkpoint(checkpoint, model, fp16, local_rank)
         print(f"resuming from {iteration} iteration")
     opt = torch.optim.Adam(model.parameters())
+
+    sampler = None
+    if args.distributed:
+        from apex.parallel import DistributedDataParallel
+        
+        # set default .to('cuda') behavior to current local_rank
+        torch.cuda.set_device(local_rank)
+        torch.distributed.init_process_group(backend='nccl', init_method='env://')
+        model = DistributedDataParallel(model, delay_allreduce=True)
+        # run with python -m torch.distributed.launch --nproc_per_node=NUM_GPUS train.py
+
+        sampler = torch.utils.data.distributed.DistributedSampler(ds)
+    
     if fp16:
         # Initialization with apex
         from apex import amp
@@ -115,34 +136,25 @@ if __name__ == "__main__":
         opt_level = 'O2'
         model, opt = amp.initialize(model, opt, opt_level=opt_level)
 
-    sampler = None
-    if args.distributed:
-        from apex.parallel import DistributedDataParallel
-
-        model = DistributedDataParallel(model, delay_allreduce=True)
-        # run with python -m torch.distributed.launch --nproc_per_node=NUM_GPUS train.py
-
-        sampler = torch.utils.data.distributed.DistributedSampler(ds)
-
     if args.sync_bn:
         from apex.parallel import convert_syncbn_model
-
-        print("using apex synced BN")
+        
+        if local_rank == 0:
+            print("using apex synced BN")
         model = convert_syncbn_model(model)
 
-    ds = TTSDataset(ds_path)
-    loader = DataLoader(ds, batch_size=batch_size, collate_fn=TTSCollate(), sampler=sampler)
-    best_metric = 1e3
+    loader = DataLoader(ds, num_workers=4, batch_size=batch_size, collate_fn=TTSCollate(), sampler=sampler)
     for epoch in range(1, num_epochs + 1):
-        print("epoch", epoch)
-        iteration, min_metric = train(model, loader, opt, device, iteration=iteration, log_every=log_every, fp16=fp16)
+        if local_rank == 0:
+            print("epoch", epoch)
+        iteration, avg_metric = train(model, loader, opt, local_rank, iteration=iteration, log_every=log_every, fp16=fp16)
         # for jupyter notebook
         # if iteration % 25 == 0:
         # clear_output(True)
         # test_alignment([ds[0]], model)
-        if args.local_rank == 0:
-            if min_metric < best_metric:
-                best_metric = min_metric
+        if local_rank == 0:
+            if avg_metric < best_metric:
+                best_metric = avg_metric
                 save_checkpoint(model, iteration, checkpoints_path.joinpath("model_best.pth"), fp16)
             if epoch % 10 == 0:
                 save_checkpoint(model, iteration, checkpoints_path.joinpath("model_last.pth"), fp16)
